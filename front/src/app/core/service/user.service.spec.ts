36084f9685d975b04a6a2893c40964faccd58ad3
Amine <moha.mohtaje@gmail.com>
2026-05-29 14:41:57 +0200

    test: update UserService tests to use HttpTestingController and provideHttpClientTesting

--- front/src/app/core/service/user.service.spec.ts
+++ front/src/app/core/service/user.service.spec.ts
@@ -1,16 +1,22 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { User } from '../models/user.interface';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
       ]
     });
     service = TestBed.inject(UserService);
